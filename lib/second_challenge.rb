ad6d5514a0a1b8e7e18dcd275a9b993f06aa065e
Nicholas-Maxwell <nick.maxwell18@gmail.com>
2016-05-23 20:44:54 -0400

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,25 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
 
-  
-end
+epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   }, 
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => [] # vale of empty array
+   }
+}
+
+        #your key/value pairs here! 
+      #your key/value pairs here! 
+   
+
+end 
+
+
+                                         
--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,30 +1,33 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
+   :patriarch => {name: "Lord Montague", age: "53"}, 
+        #your key/value pairs here 
+   :matriarch => {name: "Lady Montague", age: "54"},
         #your key/value pairs here
-        },
-      :matriarch => {
+   :hero => {name: "Romeo", age: "15", status: "alive"},
         #your key/value pairs here
-        },
-      :hero => {
-        #your key/value pairs here
-        },
-      :hero_friends => []
+   :hero_friends => []
+
    }, 
-   :capulet => {
-      :patriarch => {
+   :capulet => { 
+    :patriarch => {name: "Lord Capulet", age: "50"},
         #your key/value pairs here
-        },
-      :matriarch => {
+   :matriarch => {name: "Lady Capulet", age: "51"}, 
         #your key/value pairs here
-        },
-      :heroine => {
+   :heroine => {name: "Juliet", age: "15", status: "alive"},
         #your key/value pairs here
-        },
-      :heroine_friends => []
+   :heroine_friends => []
+
    }
+
   }
 
-  
-end
+end 
+
+#  fills out the Montague character roles keys with the key/value pairs describing their attributes
+#fills out the Capulet character rolls keys with the key/value pairs describing their attributes
+#Storing Ages as Strings because ages here r not numbers that we need 2 do math with.
+#Our hero and heroine have two friends each. That constitutes a collection of friends.
+#Since they each have a collection of friends, it makes sense to collect those friends in an array.
+#Since each friend will have his or her own attributes (name, age, etc), our array will be an array of hashes. 
