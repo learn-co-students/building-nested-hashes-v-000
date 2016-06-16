d8927b330c9ec366bff8e9f98c9bf6037f639741
Jesse Novotny <jessenovotny@gmail.com>
2016-06-16 14:06:14 -0700

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -11,7 +11,6 @@ def bonus
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
           {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
