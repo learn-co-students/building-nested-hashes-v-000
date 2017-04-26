a860ab3ce6a5289737ecc8397675c789d42ec863
Jacilyn Hayden <jacihayden@gmail.com>
2017-04-26 01:25:58 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,51 @@
+# In `lib/second_challenge.rb`, you'll find the hash you built in the previous challenge.
+# Fill out the empty hash that is the value of the `:montague` key. There are four keys in this hash:
+# * `:patriarch`
+# * `:matriarch`
+# * `:hero`
+# * `:hero_friends`
+# The first three of these keys point to a value of an empty hash. The fourth key `:hero_friends` has a value of an
+# empty array.
+# Then, do the same for the empty hash that is the value of the `:capulet` key. This time, your keys are:
+# * `:patriarch`
+# * `:matriarch`
+# * `:heroine`
+# * `:heroine_friends`
+# The values are the same as described above. Once you get this test passing, you should have a hash that looks like this:
+# ```ruby
+# epic_tragedy = {
+#    :montague => {
+#       :patriarch => {},
+#       :matriarch => {},
+#       :hero => {},
+#       :hero_friends => []
+#    },
+#    :capulet => {
+#       :patriarch => {},
+#       :matriarch => {},
+#       :heroine => {},
+#       :heroine_friends => []
+#    }
+# }
+#
+# ```
+# Now we're ready to fill out the empty hashes that constitute the values for the `:patriarch`, `:matriarch`
+# and `:hero`/`:heroine` keys, nested inside the family name keys of our epic `epic_tragedy hash`.
+#
+
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
-      }, 
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+    },
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+            }}
 
-  
 end
--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,30 +1,61 @@
+# According to the diagram that we saw at the very beginning of this exercise, each character has a set of attributes.
+# Matriarch and Patriarchs have a name and an age. The hero and heroine each have a name, age and a status.
+# In `lib/third_challenge`, you'll find the hash that you built in the previous challenge. Fill out the empty hashes that
+# are the values of the `:patriarch`, `:matriarch`, and `:hero`/`:heroine` keys with the following key/value pairs
+# * The Montague `:patriarch` has
+#   * a `:name` of "Lord Montague" and
+#   * an `:age` of "53".
+# * The Montague `:matriarch` has
+#   * a `:name` of "Lady Montague" and
+#   * an `:age` of "54".
+# * The Montague `:hero` has
+#   * a `:name` of "Romeo",
+#   * an `:age` of "15", and
+#   * a `:status` of "alive".
+# * The Capulet `:patriarch` has
+#   * a `:name` of "Lord Capulet" and
+#   * an `:age` of "50".
+# * The Capulet `:matriarch` has
+#   * a `:name` of "Lady Capulet" and
+#   * an `:age` of "51".
+# * The Capulet `:heroine` has
+#   * a `:name` of "Juliet",
+#   * an `:age` of "15", and
+#   * a `:status` of "alive".
+# Once you get this test passing, you should have the following hash:
+# ```ruby
+# epic_tragedy = {
+#    :montague => {
+#       :patriarch => {name: "Lord Montague", age: "53"},
+#       :matriarch => {name: "Lady Montague", age: "54"},
+#       :hero => {name: "Romeo", age: "15", status: "alive"},
+#       :hero_friends => []
+#    },
+#    :capulet => {
+#       :patriarch => {name: "Lord Capulet", age: "50"},
+#       :matriarch => {name: "Lady Capulet", age: "51"},
+#       :heroine => {name: "Juliet", age: "15", status: "alive"},
+#       :heroine_friends => []
+#    }
+# }
+# ```
+# We're almost done. Our hero and heroine have two friends each. That constitutes a collection of friends. Since they
+# each have a collection of friends, it makes sense to collect those friends in an array. Since each friend will have
+# his or her own attributes (name, age, etc), our array will be *an array of hashes*!
+
 def third_challenge
   epic_tragedy = {
-   :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :hero => {
-        #your key/value pairs here
-        },
-      :hero_friends => []
-   }, 
-   :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
-   }
+     :montague => {
+        :patriarch => {name: "Lord Montague", age: "53"},
+        :matriarch => {name: "Lady Montague", age: "54"},
+        :hero => {name: "Romeo", age: "15", status: "alive"},
+        :hero_friends => []
+     },
+     :capulet => {
+        :patriarch => {name: "Lord Capulet", age: "50"},
+        :matriarch => {name: "Lady Capulet", age: "51"},
+        :heroine => {name: "Juliet", age: "15", status: "alive"},
+        :heroine_friends => []
+     }
   }
-
-  
 end
