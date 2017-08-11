0494b3b3c174f43a9576ead02e262953b91edd1c
Jennifer Nelson <jennifernelson118@gmail.com>
2017-08-11 15:08:21 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,30 +1,59 @@
 def third_challenge
   epic_tragedy = {
-   :montague => {
-      :patriarch => {
-        #your key/value pairs here
+   :montague => { #1
+      :patriarch => { #1a
+        name: "Lord Montague",
+        age: "53"
         },
-      :matriarch => {
-        #your key/value pairs here
+      :matriarch => { #1b
+        name: "Lady Montague",
+        age: "54"
         },
-      :hero => {
-        #your key/value pairs here
+      :hero => { #1c
+        name: "Romeo",
+        age: "15",
+        status: "alive"
         },
-      :hero_friends => []
-   }, 
-   :capulet => {
-      :patriarch => {
-        #your key/value pairs here
+      :hero_friends => [ #1d
+        Benvolio: { #1di
+          name: "Benvolio",
+          age: "17",
+          attitude: "worried"
         },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
-   }
-  }
+        Mercutio: { #1dii
+          name: "Mercutio",
+          age: "18",
+          attitude: "hot-headed"
+        }
+        ]
+   },
+   :capulet => { #2
+      :patriarch => { #2a
+        name: "Lord Capulet",
+        age: "50",
 
-  
+        },
+      :matriarch => { #2b
+        name: "Lady Capulet",
+        age: "51"
+        },
+      :heroine => { #2c
+        name: "Juliet",
+        age: "15",
+        status: "alive"
+        },
+      :heroine_friends => [ #2d
+        Steven: { #2di
+          name: "Steven",
+          age: "30",
+          attitude: "confused"
+        },
+        Nurse: { #2dii
+          name: "Nurse",
+          age: "44",
+          attitude: "worried"
+        }
+        ]
+   } #end 2
+ } #end "epic_tragedy"
 end
