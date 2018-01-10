689c9654d83168d6a010fa1f59614f667f1fb6d7
Nick Salmon <nickmidspace@gmail.com>
2018-01-10 06:43:45 +0000

    Done.

--- spec/bonus_spec.rb
+++ spec/bonus_spec.rb
@@ -1,11 +1,30 @@
-require_relative 'spec_helper'
-require_relative '../lib/bonus.rb'
+def bonus
+  epic_tragedy = {
+   :montague => {
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => [
+         {name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Steven", age: "30", attitude: "confused"}
+      ]
+   },
+   :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [
+          {name: "Mercutio", age: "18", attitude: "hot-headed"},
+          {name: "Nurse", age: "44", attitude: "worried"}
+      ]
+   }
+  }
 
-describe "#bonus" do
-  # remove the 'x' from the 'xit' below to activate this test!
-	it "uses the []= method to change the hero and heroine's status from alive to dead" do
-    expect(bonus[:montague][:hero][:status]).to eq("dead")
-    expect(bonus[:capulet][:heroine][:status]).to eq("dead")
+  #code your solution here:
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
-  end
+
+  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
+  epic_tragedy
 end
