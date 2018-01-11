8d4274a33dd1e374d03ceb8d5319d624a067ee49
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-11 20:10:36 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,33 +1,32 @@
 def first_challenge
    epic_tragedy = {
-     :montague => {
-       :patriarch => {
-         :name => "Lord Montaque",
-         :age => "53"
-       },
-      :matriarch => {
-        :name => "Lady Montague"
-        :age => "54"
+  :montague => {
+    :patriarch => {  
+      :name => "Lord Montague",
+      :age => "53"
+  },
+    :matriarch => {
+      :name => "Lady Montague",
+      :age => "54"
+  },
+    :hero => {
+      :name => "Romeo",
+      :age => "15"
+  },
+    :hero_friends => [
+      {
+         :name => "Benvolio",
+         :age => "17",
+         :attitude => "worried"
       },
-      :hero ={
-        :name => "Romeo"
-        :age => "15"
-      },
-      :heros_friends => [
-        {
-          :name => "Benvolio"
-          :age => "17"
-          :attitude => "worried"
-        },
-        {
-          :name => "Mercutio"
-          :age => "18"
-          :attitude => "hot-headed"
-        }
-        ]
+      {
+        :name => "Mercutio",
+        :age => "18",
+        :attitude => "hot-headed"
       }
-     },
-     :capulet => {
+    ]
+  },
+  :capulet => {
     :patriarch => {
       :name => "Lord Capulet",
       :age => "50"
