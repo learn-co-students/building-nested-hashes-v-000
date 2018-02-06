5dee758529d07167703344df249cc0db3c89bfa6
Stephen Harrington <stephenharrington04@yahoo.com>
2018-02-06 17:06:41 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,36 +1,36 @@
 def third_challenge
   epic_tragedy = {
-   :montague => {
-      :patriarch => {
+    montague: {
+      patriarch: {
         name: "Lord Montague",
         age: "53"
-        },
-      :matriarch => {
+      },
+      matriarch: {
         name: "Lady Montague",
         age: "54"
-        },
-      :hero => {
+      },
+      hero: {
         name: "Romeo",
         age: "15",
         status: "alive"
-        },
-      :hero_friends => []
-   },
-   :capulet => {
-      :patriarch => {
+      },
+      hero_friends: []
+    },
+    capulet: {
+      patriarch: {
         name: "Lord Capulet",
         age: "50"
-        },
-      :matriarch => {
+      },
+      matriarch: {
         name: "Lady Capulet",
         age: "51"
-        },
-      :heroine => {
+      },
+      heroine: {
         name: "Juliet",
         age: "15",
         status: "alive"
-        },
-      :heroine_friends => []
-   }
+      },
+      heroine_friends: []
+    }
   }
 end
