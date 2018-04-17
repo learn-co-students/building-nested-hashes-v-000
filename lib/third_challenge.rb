4d97e61e7d62524b302f1987cb9abe8bd12d6fcc
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-04-17 17:45:25 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,7 +13,7 @@ def third_challenge
         },
       :hero => {
         :name => "Romeo",
-        :age => "15"
+        :age => "15",
         :status => "alive"
         #your key/value pairs here
         },
@@ -32,7 +32,7 @@ def third_challenge
         },
       :heroine => {
         :name => "Juliet",
-        :age => "15"
+        :age => "15",
         :status => "alive"
         #your key/value pairs here
         },
