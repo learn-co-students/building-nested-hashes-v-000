d03099d7f2f1e38733b03dda699f701faf12028f
Steven Crouse <sdcrouse91@gmail.com>
2018-08-20 03:07:56 +0000

    Done.

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
+         {name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
    }, 
    :capulet => {
@@ -13,11 +14,17 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+          {:name => "Steven", :age => "30", :attitude => "confused"}, 
+          {name: "Nurse" , age: "44", attitude: "worried"}
+          # The above hashes are inconsistently formatted, but they demonstrate that 
+          # the tests pass regardless of format, since both formats are valid for hashes.
       ]
    }
 }
 
-  
-
+#epic_tragedy[:montague][:patriarch][:name]  
+# Not sure why, but that line above causes a TypeError 
+# ("no implicit conversion of Symbol into Integer").
 end
+
+#fourth_challenge
