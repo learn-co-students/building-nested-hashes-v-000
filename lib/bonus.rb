b44516c267cc13dd56f5b70ac4704ea873163db0
Brad Schrag <brad.schrag@gmail.com>
2018-11-08 19:36:31 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -8,21 +8,27 @@ def bonus
          {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Steven", age: "30", attitude: "confused"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
+          {name: "Mercutio", age: "18", attitude: "hot-headed"},
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
   }
-
+  epic_tragedy.each do |family, roles|
+    roles.each do |role, values|
+      if role == :hero || role == :heroine
+        epic_tragedy[family][role][:status] = 'dead'
+      end
+    end
+  end
   #code your solution here:
 
-  
+
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
