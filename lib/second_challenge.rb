20c4477f8042c7f9ec284dd5737e8de8a56c8344
Toni-Ty <antonique2479@yahoo.com>
2019-02-10 01:51:44 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,18 +1,16 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-
-      },
-      :capulet => {
+     :montague => {
         :patriarch => {},
         :matriarch => {},
         :hero => {},
         :hero_friends => []
-      }
-    }
-  end
+     },
+     :capulet => {
+        :patriarch => {},
+        :matriarch => {},
+        :heroine => {},
+        :heroine_friends => []
+     }
+  }
 end
