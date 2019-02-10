22df59bdfdce23fe86b8218917c018c36529047a
Toni-Ty <antonique2479@yahoo.com>
2019-02-10 01:40:40 +0000

    Done.

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,19 +5,21 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
+        {name: "Benvolio", age: "17", status: "worried"}
+        {name: "Mercutio", age: "18", status: "hot-headed"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+          {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
 }
-
-  
-
+# Use this to access the name "Lord Montague"... epic_tragedy[:montague][:patriarch][:name]
+# Use this to change the hero name to "Padre" in the Montague family... epic_tragedy[:montague][:hero][:name] = "Padre"
+  end
 end
