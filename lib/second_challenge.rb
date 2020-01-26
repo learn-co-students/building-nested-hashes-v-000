942fff84db834d4f3ec64ad2528f6a715c2f4244
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-01-26 07:00:38 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,17 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+     :patriarch => "Dan",
+     :matriarch => "Anita",
+     :Hero => "Tally",
+     :hero_friends => "Stan"
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
-
-  
+      :patriarch => "George",
+      :matriarch => "Paula",
+      :Heroine => "Tino",
+      :heroine_friends => "Christian"
+      }
+    
+  }
 end
